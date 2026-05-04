a7c0646a1ae3f00dc20553a455c40ff36e5d90bc
Calvin <calvincodez@gmail.com>
2026-05-04 16:05:59 -0500

    update boiler plate

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+//Utility DOM FUNCTIONS
 const getByClass = (className) => document.getElementsByClassName(className);
 const getById = (id) => document.getElementById(id);
 const createElement = (element) => document.createElement(element);
@@ -9,7 +10,8 @@ const toggleClass = (element, className) => element.classList.toggle(className);
 const select = (selector) => document.querySelector(selector);
 const selectAll = (selector) => document.querySelectorAll(selector);
 const textContent = (element, text) => (element.textContent = text);
-
+//
+//
 const click = 'click';
 const keyup = 'keyup';
 const flexActive = 'flex-active';
