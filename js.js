da44db4ee82d2032c1aeec073cc47a30402702f5
Calvin <calvincodez@gmail.com>
2026-05-04 13:16:00 -0500

    init commit

---
+++ js.js
@@ -0,0 +1,16 @@
+const getByClass = (className) => document.getElementsByClassName(className);
+const getById = (id) => document.getElementById(id);
+const createElement = (element) => document.createElement(element);
+const appendChild = (parent, child) => parent.appendChild(child);
+const removeChild = (parent, child) => parent.removeChild(child);
+const addClass = (element, className = '') => element.classList.add(className);
+const removeClass = (element, className) => element.classList.remove(className);
+const toggleClass = (element, className) => element.classList.toggle(className);
+const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
+const textContent = (element, text) => (element.textContent = text);
+
+const click = 'click';
+const keyup = 'keyup';
+const flexActive = 'flex-active';
+const flexInactive = 'flex-inactive';
